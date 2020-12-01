aca3ef2d39ae68625032cb72beddfd31d776f0f5
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-12-01 18:30:07 +0900

    Update, no need space delete.

--- CppSource/CppSource.cpp
+++ CppSource/CppSource.cpp
@@ -5,7 +5,7 @@ int main(void)
 	try
 	{
 		time_t now = time(nullptr);
-		const tm* nowlt = localtime(&now);
+		const tm *nowlt = localtime(&now);
 
 		stringstream x;
 		x << "R0";
@@ -28,7 +28,6 @@ int main(void)
 
 		string result_reiwa = y.str();
 
-
 		stringstream z;
 		z << "20";
 		z << nowlt->tm_year - 100;
@@ -46,7 +45,7 @@ int main(void)
 		z << "秒";
 
 		string result = z.str();
-		
+
 		// uru calc
 		int redays = nowlt->tm_yday;
 		int reyear = nowlt->tm_year;
@@ -56,53 +55,53 @@ int main(void)
 		int keisan = (redays * 979 - 1033) >> 5;
 		int nichisu = (oneday + uru_keisan + keisan) / 365;
 		int redays_mini = 365 - nichisu + 27;
-		
+
 		// himekuri string
 		string tim = "時刻を表示";
 		string gantan = "来年の1月1日まであと";
 		string aisatu = " 日です";
 		string number = "日めくり数え番号";
 		string week = "曜日";
-		
+
 		// week Sun ~ Sat
 		time_t timer;
 		struct tm *t_st;
-		const char *wday[] = {"日","月","火","水","木","金","土"};
-		
+		const char *wday[] = {"日", "月", "火", "水", "木", "金", "土"};
+
 		time(&timer);
 		t_st = localtime(&timer);
-		
+
 		stringstream ss;
 		stringstream ssd;
-		
+
 		// version number
 		ss << 1;
 		ssd << 2;
 		string str_num = ss.str();
 		string str_dd = ssd.str();
 		string comma = " : ";
-		
+
 		// version number comma
 		string number_comma = ".0.";
 
-		string himekuri = number + comma + str_num + number_comma +  str_dd;
+		string himekuri = number + comma + str_num + number_comma + str_dd;
 
 		cout << tim << comma << result << comma << wday[t_st->tm_wday] << week << endl;
 		cout << gantan << comma << redays_mini << aisatu << endl;
 		cout << result_reiwa << comma << result_r << endl;
 		cout << himekuri << endl;
-		
+
 		// C++ version info.
 		long cpp17 = 201703L;
 		long cpp14 = 201402L;
 		long cpp11 = 201103L;
 		long cpp98 = 199711L;
-		
+
 		// long type → string type.
 		string cpp_ver = to_string(__cplusplus);
-		
+
 		if (cpp_ver.compare(to_string(cpp17)) == 0)
-			cout << "現在の C++ : C++17" << endl; 
+			cout << "現在の C++ : C++17" << endl;
 		else if (cpp_ver.compare(to_string(cpp14)) == 0)
 			cout << "現在の C++ : C++14" << endl;
 		else if (cpp_ver.compare(to_string(cpp11)) == 0)
@@ -111,10 +110,10 @@ int main(void)
 			cout << "現在の C++ : C++98" << endl;
 		else
 			cout << "現在の C++ : pre-standard C++" << endl;
-		
+
 		return 0;
 	}
-	catch (const exception& e)
+	catch (const exception &e)
 	{
 		cerr << e.what();
 	}
